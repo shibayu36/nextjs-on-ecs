7aac69db67f01b6f74c3a187c48f8d4d4793cfa3
shibayu36 <shibayu36@gmail.com>
2020-02-08 15:28:28 +0900

    deploy.shでecspressoを使ってデプロイもするように

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ export DOCKER_BUILDKIT=1
 rm -rf ./dist/
 mkdir ./dist/
 
-REVISION="$(git rev-parse --short HEAD)"
+export REVISION="$(git rev-parse --short HEAD)"
 
 docker build --file Dockerfile.server --target nextjs-on-ecs-server --tag "nextjs-on-ecs-server:$REVISION" --progress plain .
 
@@ -19,3 +19,5 @@ echo "648803025740.dkr.ecr.ap-northeast-1.amazonaws.com/nextjs-on-ecs-server:$RE
 CONTAINER_ID="$(docker create "nextjs-on-ecs-server:$REVISION")"
 docker cp ${CONTAINER_ID}:/app/.next ./dist/
 docker rm -v ${CONTAINER_ID}
+
+ecspresso --config config.yaml deploy
