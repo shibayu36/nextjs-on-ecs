82f4f4937ef64257bb0d6b77347e49a0384b4cf9
shibayu36 <shibayu36@gmail.com>
2020-02-10 22:26:06 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { NextPage } from "next";
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1>Hello Next.js 👋Updated</h1>
       <p>
         <Link href="/about">
           <a>About</a>
