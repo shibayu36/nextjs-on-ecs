f47235a5c7562cedf0f56fae31bf48109b8da1f7
shibayu36 <shibayu36@gmail.com>
2020-02-14 10:16:30 +0900

    cdk.context.jsonを無理やり更新

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,6 @@ aws s3 sync ./dist/.next/static s3://nextjs-on-ecs-static-bucket/_next/static
 
 # Nextのサーバが動くECSを更新
 yarn workspace cdk run cdk deploy --context application-version=$REVISION NextServerStack
+
+# cdk.context.jsonをデプロイしたrevisionに更新しておく
+sed -i '' -E "s/\"application-version\": \"[^\"]+\"/\"application-version\": \"$REVISION\"/" ./cdk/cdk.context.json
